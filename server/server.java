c178d58627bbd789754c5389bfe5bcd81641925a
xylafur <jKesleyRichardson@gmail.com>
2018-08-18 00:53:34 -0500

    Added basic client server relation

---
+++ server/server.java
@@ -0,0 +1,33 @@
+/*  This is the main startup file for the world combat game, it will initialize
+ *  the server to start accepting client connections
+ *
+ */
+import java.io.*;
+import java.net.*;
+
+class Server {
+    public DataInputStream getDataInputStream(int port) throws IOException {
+        ServerSocket ss = new ServerSocket(port);
+        Socket s = ss.accept(); //establishes connection
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        return dis;
+    }
+
+    public void readFromClient(DataInputStream dis) throws IOException {
+        String str = (String)dis.readUTF();
+        System.out.println(str);
+    }
+
+}
+
+class Main {
+    public static void main(String args []){
+        try {
+            Server server = new Server();
+            DataInputStream dis = server.getDataInputStream(6666);
+            server.readFromClient(dis);
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
